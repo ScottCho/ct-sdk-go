380a8a9405912705a1b6e43c6a298bd6ccb0d957
ScottCho <scottcho@qq.com>
2023-02-21 10:22:34 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module ct-sdk-go
+
+go 1.18
+
+require github.com/jmespath/go-jmespath v0.4.0
---
+++ service/sts/customizations.go
@@ -0,0 +1,11 @@
+package sts
+
+import "ct-sdk-go/aws/request"
+
+func init() {
+	initRequest = customizeRequest
+}
+
+func customizeRequest(r *request.Request) {
+	r.RetryErrorCodes = append(r.RetryErrorCodes, ErrCodeIDPCommunicationErrorException)
+}
